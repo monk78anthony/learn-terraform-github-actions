7d2477ac5b143a15e5c33230ed761ed188f6503f
Ant Andrades <anthony.andrades@schneider-electric.com>
2022-04-26 21:13:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,9 +79,9 @@ resource "aws_db_instance" "example" {
   engine              = "mysql"
   allocated_storage   = 10
   instance_class      = "db.t2.micro"
-  name                = var.db_name
-  username            = "admin"
-  password            = var.db_password
+  name                = "learning"
+  username            = "foo"
+  password            = "foobarbaz"
   skip_final_snapshot = true
 }
 
