857a18f704de7c9dcdbfbdea43dcff586713334c
Ant Andrades <antandrades@gmail.com>
2022-10-06 12:43:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+      source = "hashicorp/aws"
+      version = "4.33.0"
     }
     random = {
       source  = "hashicorp/random"
